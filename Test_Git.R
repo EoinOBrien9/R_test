15a24a176c46ea9728803f0174f8a71b25d08856
eoin.obrien <eoin.obrien@afcb.co.uk>
2024-04-29 10:37:52 +0100

    tesing first commit

---
+++ Test_Git.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(ggplot2)
+library(ggtext)
+library(grid)
+library(png)
+library(showtext)
+library(data.table)
+library(shiny)
+library(DBI)
+library(RPostgres)
+library(stringr)
+library(ggiraph)
+library(glue)
+library(ggnewscale)
+library(tidyr)
+library(tableHTML)
+library(cowplot)
+library(lubridate)
+library(DT)
+library(parallel)
+library(doParallel)
+library(scales)
+library(ggrepel)
+library(StatsBombR)
+
+
+setwd('/Users/eoin.obrien/Documents/Git')
+
+username <- "eoin.obrien@afcb.co.uk"
+password <- "uoWde2CZ"
+competitions <- competitions(username, password)
